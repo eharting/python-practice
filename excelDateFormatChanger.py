6d654386ff5466a05bf43947a41e72b5f27094f8
Eric Harting <eric.harting@gmail.com>
2017-03-15 08:07:43 -0400

    Create excelDateFormatChanger.py

---
+++ excelDateFormatChanger.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+os.chdir('path')
+files = os.listdir(os.getcwd())
+
+for file in files:
+    df = pd.read_csv(file, names = ['meter','date','value'])
+
+    frmt0 = '%Y-%m-%d %H:%M:%S.%f'
+    frmt1 = '%m/%d/%Y %H:%M:%S'
+
+    df['date'] = pd.to_datetime(df.date, format = frmt0)
+    df['date'] = df['date'].dt.strftime(frmt1)
+
+    df.to_csv('Edited_'+file, header=None, index = None)
